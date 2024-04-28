e337231b9dc00742e217582a07f7179dc5d715bf
Noufal <noufal@n.com>
2024-04-28 14:49:06 +0530

    Update task factory with fake

--- project_managment/database/factories/TaskFactory.php
+++ project_managment/database/factories/TaskFactory.php
@@ -17,7 +17,15 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->sentence(),
+            'description' => fake()->realText(),
+            'due_date' => fake()->dateTimeBetween('now', '+1 year'),
+            'status' => fake()->randomElement(['Not Started', 'In Progress', 'Completed']),
+            'priority' => fake()->randomElement(['Low', 'Medium', 'High']),
+            'image_path' => fake()->imageUrl(),
+            'assigned_user_id' => 1,
+            'created_at' => 1,
+            'updated_at' => 1
         ];
     }
 }
